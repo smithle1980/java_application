1879f92d8a8f775fb6e20929872a0478210ef816
smithle1980 <102009526+smithle1980@users.noreply.github.com>
2022-05-02 22:20:34 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,11 @@ public class App
 {
 
     public long add(long first, long second) {
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;    
         return first + second;
     }
 
